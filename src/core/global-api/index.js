23cd361669fb7a9118ac6e7e458b522a156671d3
lechan <lechan@126.com>
2020-08-29 23:49:14 +0800

    comment：Vue组件Component源码解析

--- src/core/global-api/index.js
+++ src/core/global-api/index.js
@@ -29,11 +29,13 @@ export function initGlobalAPI (Vue: GlobalAPI) {
       )
     }
   }
+  // 初始化 Vue.config 对象
   Object.defineProperty(Vue, 'config', configDef)
 
   // exposed util methods.
   // NOTE: these are not considered part of the public API - avoid relying on
   // them unless you are aware of the risk.
+  // 这些工具方法不视作全局API的一部分，除非你已经意识到某些风险，否则不要去依赖他们
   Vue.util = {
     warn,
     extend,
@@ -60,10 +62,15 @@ export function initGlobalAPI (Vue: GlobalAPI) {
   // components with in Weex's multi-instance scenarios.
   Vue.options._base = Vue
 
+  // 设置 keep-alive 组件
   extend(Vue.options.components, builtInComponents)
 
+  // 注册 Vue.use() 用来注册插件
   initUse(Vue)
+  // 注册 Vue.mixin() 实现混入
   initMixin(Vue)
+  // 注册 Vue.extend() 基于传入的options返回一个组件的构造函数
   initExtend(Vue)
+  // 注册 Vue.directive()、Vue.component()、Vue.filter()
   initAssetRegisters(Vue)
 }
